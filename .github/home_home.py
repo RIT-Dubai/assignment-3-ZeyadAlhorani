9730bf6fc17f4e9c3b7ee16132e3e33144e225dc
Zeyad <zwa2861@rit.edu>
2021-11-14 11:53:42 +0400

    3rd commit

--- .github/home_home.py
+++ .github/home_home.py
@@ -1,11 +1,32 @@
 
 class home:
+    "return"
+
+class home_category:
+    ":return"
+
+# these dictionaries are only examples
+garden_items = {"G1": "Garden Hose", "G2": "Soil", "G3": "Water"}
+indooe_items = {"I1": "Couch", "I2": "Rug", "I3": "Table"}
+bathroom_items = {"B1": "Bathroom Pipe", "B2":"Bathroom Rug", "B3": "Bathroom Curtain"}
+
+
+class item:
+# a class that identifies an item
     def __init__ (self, name, letter_code, price):
      self.name = " "
      self.letter_code = " "
-     self.price = " "
+     self.price = 0
 
 
+garden_ant = {"Garden Plant", "F1", 20}
+
+class avatar:
+    def __init__(self, category, base_charge):
+      self.category = " "
+      self.base_charge = 50
 
 
-
+def make_order():
+# function that allows the user to order
+    return
