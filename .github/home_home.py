248a043655597be0c1add6d639fcc35aeff0459b
Zeyad <zwa2861@rit.edu>
2021-11-21 12:06:29 +0400

    9th commit

--- .github/home_home.py
+++ .github/home_home.py
@@ -1,20 +1,8 @@
 
 
-garden_items = {"Garden Hose": ("G1", 15), "Soil": ("G2", 5), "Tools": ("G3", 30)}
-indoor_items = {"Couch": ("I1", 250), "Rug": ("I2", 85), "Table": ("I3", 170)}
-bathroom_items = {"Bathroom Mirror": ("B1",50), "Bathroom Rug": ("B2", 20), "Bathroom Curtain": ("B3", 10)}
-
-def inputs():
-    command = input()
-    if command == "G":
-        print(print_garden())
-
-
-    if command == "I":
-        print(print_indoor())
-
-    if command == "B":
-        print(print_bathroom())
+garden_items = {"G1": ("Garden Hose", 15), "G2": ("Soil", 5), "G3": ("Tools", 30)}
+indoor_items = {"I1": ("Couch", 250), "I2": ("Rug", 85), "I3": ("Table", 170)}
+bathroom_items = {"B1": ("Bathroom Mirror",50), "B2": ("Bathroom Rug", 20), "B3": ("Bathroom Curtain", 10)}
 
 
 class Home:
@@ -26,39 +14,16 @@ class Home:
      self.letter_code = " "
      self.price = " "
 
-    garden_items = {"Garden Hose": ("G1", 15), "Soil": ("G2", 5), "Tools": ("G3", 30)}
-    indoor_items = {"Couch": ("I1", 250), "Rug": ("I2", 85), "Table": ("I3", 170)}
-    bathroom_items = {"Bathroom Mirror": ("B1",50), "Bathroom Rug": ("B2", 20), "Bathroom Curtain": ("B3", 10)}
+    garden_items = {"G1": ("Garden Hose", 15), "G2": ("Soil", 5), "G3": ("Tools", 30)}
+    indoor_items = {"I1": ("Couch", 250), "I2": ("Rug", 85), "I3": ("Table", 170)}
+    bathroom_items = {"B1": ("Bathroom Mirror",50), "B2": ("Bathroom Rug", 20), "B3": ("Bathroom Curtain", 10)}
 
 
 
-class Home_Category:
-    # these dictionaries are only examples for now
-    garden_items = {"Garden Hose": ("G1", 15), "Soil": ("G2", 5), "Tools": ("G3", 30)}
-    indoor_items = {"Couch": ("I1", 250), "Rug": ("I2", 85), "Table": ("I3", 170)}
-    bathroom_items = {"Bathroom Mirror": ("B1",50), "Bathroom Rug": ("B2", 20), "Bathroom Curtain": ("B3", 10)}
+class Home_Avatar:
+    # these dictionaries are only examples for no
+    pass
 
-"""""
-class item:
-# a class that identifies an item
-    __slots__ = ("name", "letter_code", "price")
-    def __init__ (self, name, letter_code, price):
-     self.name = " "
-     self.letter_code = " "
-     self.price = 0
-
-"""""
-garden_ant = {"Garden Plant", "F1", 20}
-
-class Avatar:
-    def __init__(self, category, base_charge):
-      self.category = " "
-      self.base_charge = 50
-
-
-def make_order():
-# function that allows the user to orders
-    return
 
 
 def print_garden():
@@ -84,22 +49,51 @@ def main():
     #Home()
     print("Welcome to Home Ideas Center, where all orders include a new home feeling!")
     print("For your new Home space ...")
-    print(" Choose a letter to display option for each category")
-    print("    G for Garden Items   ", "   I for Indoor Items   ",   "B for Bathroom Items   ")
 
-    command = input()
-    if command == "G":
-        print(print_garden())
+    def options():
+        print(" Choose a letter to display option for each category")
+        print("    G for Garden Items   ", "   I for Indoor Items   ",   "B for Bathroom Items   ")
 
-    if command == "I":
-        print(print_indoor())
+        cart = 0
 
-    if command == "B":
-        print(print_bathroom())
+        command = input()
+        if command == "G":
+           print(print_garden())
+           pass
+           options()
 
+        if command == "I":
+            print(print_indoor())
+            options()
 
+        if command == "B":
+            print(print_bathroom())
+            pass
+            options()
 
+        elif command == "G1":
+            cart += 15
+            print("You added Garden Hose to your cart, total is:", cart)
+            options()
 
+        elif command == "G2":
+            cart += 5
+            print("You added Soil to your cart, total is:", cart)
+            options()
+
+        elif command == "G3":
+            price = garden_items["G3"][1]
+            cart =+ price
+            print("You added Tools to your cart, total is:", cart)
+            options()
+
+    options()
+
+"""""
+garden_items = {"G1": ("Garden Hose", 15), "G2": ("Soil", 5), "G3": ("Tools", 30)}
+    indoor_items = {"I1": ("Couch", 250), "I2": ("Rug", 85), "I3": ("Table", 170)}
+    bathroom_items = {"B1": ("Bathroom Mirror",50), "B2": ("Bathroom Rug", 20), "B3": ("Bathroom Curtain", 10)}
+"""""
 
 
 
